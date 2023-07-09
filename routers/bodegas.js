a976b3b8948d17d97684bdb8ae341abeda629356
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-09 18:59:11 -0500

    feat: ✨ Implement a new Endpoint for bodegas
    Implement a new Endpoint for post new data in "bodega" into database

--- routers/bodegas.js
+++ routers/bodegas.js
@@ -16,4 +16,21 @@ bodegasStorage.get("/",(req, res)=>{
     })
 })
 
+bodegasStorage.post("/", (req, res) => {
+    let myData = req.body;
+    let data = Object.values(myData);
+  
+    connection.query(
+      'INSERT INTO `bodegas`(`id`, `nombre`, `estado`, `created_at`, `updated_at`, `deleted_at`,`id_responsable`, `created_by`, `update_by`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      data,
+      (err, result, fields) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Error al insertar en la base de datos");
+        }else{
+            res.end("Los datos fueron insertados con exito en la base de datos");
+        }
+    });
+  });
+
 export default bodegasStorage;
