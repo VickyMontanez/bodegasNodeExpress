16f5238344715c9fe1e65edc4c751c9e1d62fb47
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-09 22:45:59 -0500

    feat: ✨ Implement a new Endpoint for inventarios
    Implement a new Endpoint to publish a new inventory and insert data with the input parameters

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import bodegasStorage from "./routers/bodegas.js";
 import productosStorage from "./routers/productos.js";
+import storageInventarios from "./routers/inventarios.js";
 
 const appExpress = express();
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 appExpress.use(express.json());
 appExpress.use('/bodegas', bodegasStorage);
 appExpress.use('/productos', productosStorage);
+appExpress.use('/inventarios', storageInventarios);
 
 /* Se levanta el servidor */
 const config = JSON.parse(process.env.MY_CONFIG);
