24c4c153b9e82793a160457d39eb101f41e196a3
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-09 22:24:26 -0500

    feat: ✨ Implement a new Endpoint for productos
    Implement a new Endpoint to post a new product  and insert data in inventario's table

--- routers/productos.js
+++ routers/productos.js
@@ -16,4 +16,47 @@ productosStorage.get("/",(req, res)=>{
     })
 })
 
+productosStorage.post('/', (req, res) => {
+    let myData = req.body;
+    let data = Object.values(myData);
+
+    connection.query('INSERT INTO productos (id, nombre, descripcion, estado, created_at, updated_at, deleted_at, created_by, update_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        data,
+        (err, productResult) => {
+            if (err) {
+                console.error("Error al insertar producto: ", err);
+                return res.status(500).json({ mensaje: "Error al insertar producto" });
+            }
+
+            const id_producto = req.body.id;
+            const initial_amount = 10;
+            const created_by = null;
+            const update_by = null;
+            let newId;
+
+            connection.query('SELECT MAX(id) AS lastId FROM bodegas', (err, result) => {
+                if (err) {
+                    console.error('Error al obtener el último ID:', err);
+                    return;
+                }
+                const lastId = result[0].lastId
+                newId = lastId;
+
+                connection.query(`INSERT INTO inventarios (id_bodega, id_producto, id, cantidad, created_by, update_by, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, NOW(), NOW())`,
+                    [newId, id_producto, newId, initial_amount, created_by, update_by],
+                    (err, inventoryResult) => {
+                        if (err) {
+                            console.error("Error al insertar inventario: ", err);
+                            return res.status(500).json({ mensaje: "Error al insertar inventario" });
+                        } else {
+                            return res.json({ mensaje: "Producto insertado exitosamente" });
+                        }
+                    }
+                );
+            });
+
+        }
+    );
+});
+
 export default productosStorage;
