cd4aa0b6f337746f281a88c5ef3df6a6163af2f3
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-09 19:48:15 -0500

    feat: ✨ Implement a new Endpoint for productos
    Implement a new Endpoint to get the total of products in the database

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import bodegasStorage from "./routers/bodegas.js";
+import productosStorage from "./routers/productos.js";
 
 const appExpress = express();
 dotenv.config();
 
 appExpress.use(express.json());
 appExpress.use('/bodegas', bodegasStorage);
+appExpress.use('/productos', productosStorage);
 
 /* Se levanta el servidor */
 const config = JSON.parse(process.env.MY_CONFIG);
