92bb447d853f22d180f2cda8c80f3863ac21c35b
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-17 12:14:22 -0500

    feat: ✨ Basic Config for tests
    Basic Config for tests

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ import productosStorage from "./routers/productos.js";
 import storageInventarios from "./routers/inventarios.js";
 import historialesStorage from "./routers/historiales.js";
 import usersStorage from "./routers/users.js";
-
 import "reflect-metadata";
-/* import {plainToClass} from "class-transformer";
-import {bodegas} from "./controllerDto/bodegas.js";
-import {inventarios} from "./controllerDto/inventarios.js";
-import {users} from "./controllerDto/users.js"; */
+
 
 const appExpress = express();
 dotenv.config();
@@ -28,22 +24,4 @@ appExpress.listen(config, ()=>{
     console.log(`http://${config.hostname}:${config.port}`);
 });
 
-/* let json = {
-    "id": 12,
-    "nombre": "Vicky Vanessa Montañez Molina",
-    "email": "vmontanez707@gmail.com",
-    "verify_email": "vmontanez707@gmail.com",
-    "estado": 1,
-    "created_by": 3,
-    "update_by": 5,
-    "foto": "https://f/_c265d.jpg",
-    "contraseña": "A235674Gh",
-    "created_at": null,
-    "updated_at": null,
-    "deleted_at": null
-
-};
-
-let data = plainToClass(users, json, {excludeExtraneousValues: true});
-console.log(data); */
 
