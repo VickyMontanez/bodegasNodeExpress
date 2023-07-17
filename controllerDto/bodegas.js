566f75048c90b2d0e7f87d939b001f9ce08ced1d
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-07-17 10:56:45 -0500

    feat: ✨ Implement DTO and middleware of the bodega's table
    A good implementation of the dto and middleware was made for the bodegas table

--- controllerDto/bodegas.js
+++ controllerDto/bodegas.js
@@ -7,7 +7,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-import { Expose, Transform } from 'class-transformer';
+import { Expose, Type, Transform } from 'class-transformer';
+import { IsDefined, MaxLength, IsString, IsInt, Matches, IsOptional } from "class-validator";
 export class bodegas {
     constructor(ID, nom_user, responsableID, estado_user, createdBy, updateBy, createdAt, updatedAt, deletedAt) {
         this.ID = ID;
@@ -23,71 +24,62 @@ export class bodegas {
 }
 __decorate([
     Expose({ name: 'id' }),
-    Transform(({ value }) => {
-        if (Math.floor(value) && typeof value == "number")
-            return Math.floor(value);
-        else
-            throw { status: 400, message: "Los datos del Id no cumple con los párametros establecidos" };
-    }, { toClassOnly: true }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! El parametro Id es obligatorio' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! El parametro Id no cumple con el tipo de dato establecido' }; } }),
+    Type(() => Number),
     __metadata("design:type", Number)
 ], bodegas.prototype, "ID", void 0);
 __decorate([
     Expose({ name: 'nombre' }),
-    Transform(({ value }) => { if (/^[A-Z][a-zA-Z '.-]*[A-Za-z][^-]\w+[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]/.test(value))
-        return value;
-    else
-        throw { status: 400, message: "Los datos del nombre no cumplen con los párametros establecidos" }; }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! El parametro nombre del usuario es obligatorio' }; } }),
+    IsString({ message: () => { throw { status: 401, message: '¡ERROR! El parametro nombre del usuario no cumple con el tipo de dato establecido' }; } }),
+    MaxLength(50, { message: () => { throw { status: 401, message: '¡ERROR! El parametro nombre del usuario superó el limite de carácteres' }; } }),
+    Matches(/^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]+$/, { message: () => { throw { status: 401, message: '¡ERROR! El parámetro nombre del usuario contiene caracteres no válidos' }; } }),
+    Type(() => String),
     __metadata("design:type", String)
 ], bodegas.prototype, "nom_user", void 0);
 __decorate([
     Expose({ name: 'id_responsable' }),
-    Transform(({ value }) => {
-        if (Math.floor(value) && typeof value == "number")
-            return Math.floor(value);
-        else
-            throw { status: 400, message: "Los datos del Responsable no cumple con los párametros establecidos" };
-    }, { toClassOnly: true }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! El parametro id_responsable es obligatorio' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! El parametro id_responsable no cumple con el tipo de dato establecido' }; } }),
+    Type(() => Number),
     __metadata("design:type", Number)
 ], bodegas.prototype, "responsableID", void 0);
 __decorate([
     Expose({ name: 'estado' }),
-    Transform(({ value }) => {
-        if (Math.floor(value) && typeof value == "number")
-            return Math.floor(value);
-        else
-            throw { status: 400, message: "Los datos del estado no cumple con los párametros establecidos" };
-    }, { toClassOnly: true }),
+    IsDefined({ message: () => { throw { status: 401, message: '¡ERROR! El parametro estado es obligatorio' }; } }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! El parametro estado no cumple con el tipo de dato establecido' }; } }),
+    Type(() => Number),
     __metadata("design:type", Number)
 ], bodegas.prototype, "estado_user", void 0);
 __decorate([
     Expose({ name: 'created_by' }),
-    Transform(({ value }) => {
-        if (Math.floor(value) && typeof value == "number")
-            return Math.floor(value);
-        else
-            throw { status: 400, message: "Los datos del created_by no cumple con los párametros establecidos" };
-    }, { toClassOnly: true }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! El parametro created_by no cumple con el tipo de dato establecido' }; } }),
+    Type(() => Number),
     __metadata("design:type", Number)
 ], bodegas.prototype, "createdBy", void 0);
 __decorate([
     Expose({ name: 'update_by' }),
-    Transform(({ value }) => {
-        if (Math.floor(value) && typeof value == "number")
-            return Math.floor(value);
-        else
-            throw { status: 400, message: "Los datos del update_by no cumple con los párametros establecidos" };
-    }, { toClassOnly: true }),
+    IsInt({ message: () => { throw { status: 401, message: '¡ERROR! El parametro created_by no cumple con el tipo de dato establecido' }; } }),
+    Type(() => Number),
     __metadata("design:type", Number)
 ], bodegas.prototype, "updateBy", void 0);
 __decorate([
     Expose({ name: 'created_at' }),
+    IsOptional(),
+    Transform(({ value }) => new Date(value)),
+    Type(() => Date),
     __metadata("design:type", Date)
 ], bodegas.prototype, "createdAt", void 0);
 __decorate([
     Expose({ name: 'updated_at' }),
+    Transform(({ value }) => new Date(value)),
+    Type(() => Date),
     __metadata("design:type", Date)
 ], bodegas.prototype, "updatedAt", void 0);
 __decorate([
     Expose({ name: 'deleted_at' }),
+    Transform(({ value }) => new Date(value)),
+    Type(() => Date),
     __metadata("design:type", Date)
 ], bodegas.prototype, "deletedAt", void 0);
